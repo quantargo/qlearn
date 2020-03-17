81bb96540625daeb23d4df6ea1426f7a6f3708d7
mario <mario.annau@quantargo.com>
2020-03-17 12:11:01 +0100

    Adjust ASSETS_URL

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,16 @@
   install_knitr_hooks()
   learnr:::remove_knitr_hooks()
   options(STAGE = Sys.getenv("STAGE", "dev"))
-  if (options("STAGE") == "dev") {
-    options(ASSETS_URL = "https://next.quantargo.com/assets/courses")
-  } else if (options("STAGE") == "prod") {
-    options(ASSETS_URL = "https://www.quantargo.com/assets/courses")
+
+  assets_url <- Sys.getenv("ASSETS_URL")
+  if (assets_url == "") {
+    if (options("STAGE") == "dev") {
+      options(ASSETS_URL = "https://next.quantargo.com/assets/courses")
+    } else if (options("STAGE") == "prod") {
+      options(ASSETS_URL = "https://www.quantargo.com/assets/courses")
+    }
+  } else {
+    options(ASSETS_URL = assets_url)
   }
 }
 
