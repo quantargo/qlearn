60ef3a5872bebfa937ed2b321f84e3220a61a2d6
mannau <mario.annau@quantargo.com>
2021-05-12 15:39:21 +0200

    Optionally get ASSETS_URL from env (for blog)

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,12 @@
 .onAttach <- function(libname, pkgname) {
   install_knitr_hooks()
   learnr:::remove_knitr_hooks()
-  options(STAGE = Sys.getenv("STAGE", "dev"))
+  stage = Sys.getenv("STAGE", "dev")
+  options(STAGE = stage)
 
-  assets_url <- Sys.getenv("ASSETS_URL")
-  if (assets_url == "") {
-    options(ASSETS_URL = "/assets/courses")
-  } else {
-    options(ASSETS_URL = assets_url)
-  }
+  url_prefix <- if(stage == "prod") "https://cdn.quantargo.com" else "https://cdn-next.quantargo.com"
+  assets_url <- file.path(url_prefix, Sys.getenv("ASSETS_URL", "assets/courses"))
+  options(ASSETS_URL = assets_url)
   options(width=69)
 
   assign("question", qlearn::question, envir = .GlobalEnv)
